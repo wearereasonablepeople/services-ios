da57e1c39b600b2521f6b5582a13cfb597601237
Oleksii Dykan <alick_bass@mail.ru>
2016-07-27 11:56:20 +0200

    Add Segue Handler to WARPServices Framework

---
+++ WARPServices/SegueHandler.swift
@@ -0,0 +1,27 @@
+//
+//  SegueHandler.swift
+//  WARPServices
+//
+//  Created by Oleksii on 22/06/16.
+//  Copyright © 2016 Oleksii Dykan. All rights reserved.
+//
+
+import UIKit
+
+public protocol SegueHandlerType {
+    associatedtype SegueIdentifier: RawRepresentable
+}
+
+public extension SegueHandlerType where Self: UIViewController, SegueIdentifier.RawValue == String {
+    func performSegue(with segueIdentifier: SegueIdentifier, sender: AnyObject?) {
+        performSegueWithIdentifier(segueIdentifier.rawValue, sender: sender)
+    }
+    
+    func segueIdentifier(`for` segue: UIStoryboardSegue) -> SegueIdentifier {
+        guard let identifier = segue.identifier, segueIdentifier = SegueIdentifier(rawValue: identifier) else {
+            fatalError("Invalid segue identifier \(segue.identifier).")
+        }
+        
+        return segueIdentifier
+    }
+}
