f63fbe561bfce81fda0e4c843d500bbf72f76485
Oleksii Dykan <alick_bass@mail.ru>
2016-07-27 11:50:06 +0200

    Initial Commit

---
+++ WARPServices/WARPServices.h
@@ -0,0 +1,19 @@
+//
+//  WARPServices.h
+//  WARPServices
+//
+//  Created by Oleksii on 27/07/16.
+//  Copyright © 2016 Oleksii Dykan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WARPServices.
+FOUNDATION_EXPORT double WARPServicesVersionNumber;
+
+//! Project version string for WARPServices.
+FOUNDATION_EXPORT const unsigned char WARPServicesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WARPServices/PublicHeader.h>
+
+
