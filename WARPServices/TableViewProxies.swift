a191bb7a1d20c2a676336af6f076f36418b9ae79
Oleksii Dykan <alick_bass@mail.ru>
2016-09-26 10:11:52 +0200

    Add TableViewDataSource protocol
    TableViewDataSource protocol is duplicate of UITableViewDataSource but is purely swift.

--- WARPServices/TableViewProxies.swift
+++ WARPServices/TableViewProxies.swift
@@ -6,4 +6,13 @@
 //  Copyright © 2016 Oleksii Dykan. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+public protocol TableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String?
+    func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String?
+}
+
