0166c64533cd17fec3963acdeb80eec09f97c1fb
Oleksii Dykan <alick_bass@mail.ru>
2016-11-21 17:18:53 +0100

    Add TableViewDelegate proxy

--- WARPServices/TableViewDelegateProxy.swift
+++ WARPServices/TableViewDelegateProxy.swift
@@ -35,8 +35,128 @@ public protocol TableViewDelegate {
     // Selection
     func tableView(_ tableView: UITableView, didHighlightRowAt indexPath: IndexPath)
     func tableView(_ tableView: UITableView, didUnhighlightRowAt indexPath: IndexPath)
-    func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath?
-    func tableView(_ tableView: UITableView, willDeselectRowAt indexPath: IndexPath) -> IndexPath?
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
     func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath)
+}
+
+public extension TableViewDelegate {
+    
+    // Display customization
+    public func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {}
+    public func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {}
+    public func tableView(_ tableView: UITableView, willDisplayFooterView view: UIView, forSection section: Int) {}
+    public func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {}
+    public func tableView(_ tableView: UITableView, didEndDisplayingHeaderView view: UIView, forSection section: Int) {}
+    public func tableView(_ tableView: UITableView, didEndDisplayingFooterView view: UIView, forSection section: Int) {}
+    
+    // Variable height support
+    public func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat { return tableView.rowHeight }
+    public func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat { return tableView.sectionHeaderHeight }
+    public func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat { return tableView.sectionFooterHeight }
+    
+    public func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return tableView.estimatedRowHeight
+    }
+    
+    public func tableView(_ tableView: UITableView, estimatedHeightForHeaderInSection section: Int) -> CGFloat {
+        return tableView.estimatedSectionHeaderHeight
+    }
+    
+    public func tableView(_ tableView: UITableView, estimatedHeightForFooterInSection section: Int) -> CGFloat {
+        return tableView.estimatedSectionFooterHeight
+    }
+    
+    // Section header & footer information. Views are preferred over title should you decide to provide both
+    public func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? { return nil }
+    public func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? { return nil }
+    public func tableView(_ tableView: UITableView, accessoryButtonTappedForRowWith indexPath: IndexPath) {}
+    
+    // Selection
+    public func tableView(_ tableView: UITableView, didHighlightRowAt indexPath: IndexPath) {}
+    public func tableView(_ tableView: UITableView, didUnhighlightRowAt indexPath: IndexPath) {}
+    public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {}
+    public func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {}
+}
+
+public class TableViewDelegateProxy: NSObject, UITableViewDelegate {
+    public weak var delegate: (AnyObject & TableViewDelegate)?
+    
+    // Display customization
+    public func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        delegate?.tableView(tableView, willDisplay: cell, forRowAt: indexPath)
+    }
+    
+    public func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        delegate?.tableView(tableView, willDisplayHeaderView: view, forSection: section)
+    }
+    
+    public func tableView(_ tableView: UITableView, willDisplayFooterView view: UIView, forSection section: Int) {
+        delegate?.tableView(tableView, willDisplayFooterView: view, forSection: section)
+    }
+    
+    public func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        delegate?.tableView(tableView, didEndDisplaying: cell, forRowAt: indexPath)
+    }
+    
+    public func tableView(_ tableView: UITableView, didEndDisplayingHeaderView view: UIView, forSection section: Int) {
+        delegate?.tableView(tableView, didEndDisplayingHeaderView: view, forSection: section)
+    }
+    
+    public func tableView(_ tableView: UITableView, didEndDisplayingFooterView view: UIView, forSection section: Int) {
+        delegate?.tableView(tableView, didEndDisplayingFooterView: view, forSection: section)
+    }
+    
+    public func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return delegate?.tableView(tableView, heightForRowAt: indexPath) ?? tableView.rowHeight
+    }
+    
+    public func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return delegate?.tableView(tableView, heightForHeaderInSection: section) ?? tableView.sectionHeaderHeight
+    }
+    
+    public func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return delegate?.tableView(tableView, heightForFooterInSection: section) ?? tableView.sectionFooterHeight
+    }
+    
+    public func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return delegate?.tableView(tableView, estimatedHeightForRowAt: indexPath) ?? tableView.estimatedRowHeight
+    }
+    
+    public func tableView(_ tableView: UITableView, estimatedHeightForHeaderInSection section: Int) -> CGFloat {
+        return delegate?.tableView(tableView, estimatedHeightForHeaderInSection: section) ?? tableView.estimatedSectionHeaderHeight
+    }
+    
+    public func tableView(_ tableView: UITableView, estimatedHeightForFooterInSection section: Int) -> CGFloat {
+        return delegate?.tableView(tableView, estimatedHeightForFooterInSection: section) ?? tableView.estimatedSectionFooterHeight
+    }
+    
+    // Section header & footer information. Views are preferred over title should you decide to provide both
+    public func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        return delegate?.tableView(tableView, viewForHeaderInSection: section)
+    }
+    
+    public func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        return delegate?.tableView(tableView, viewForFooterInSection: section)
+    }
+    
+    public func tableView(_ tableView: UITableView, accessoryButtonTappedForRowWith indexPath: IndexPath) {
+        delegate?.tableView(tableView, accessoryButtonTappedForRowWith: indexPath)
+    }
+    
+    // Selection
+    public func tableView(_ tableView: UITableView, didHighlightRowAt indexPath: IndexPath) {
+        delegate?.tableView(tableView, didHighlightRowAt: indexPath)
+    }
+    
+    public func tableView(_ tableView: UITableView, didUnhighlightRowAt indexPath: IndexPath) {
+        delegate?.tableView(tableView, didUnhighlightRowAt: indexPath)
+    }
+    
+    public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        delegate?.tableView(tableView, didSelectRowAt: indexPath)
+    }
+    
+    public func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        delegate?.tableView(tableView, didDeselectRowAt: indexPath)
+    }
 }
