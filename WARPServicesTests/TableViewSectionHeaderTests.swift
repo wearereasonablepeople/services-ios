773ebaba34a0df2d379ff2637d242afea976ebfd
Oleksii Dykan <alick_bass@mail.ru>
2017-03-31 10:08:16 +0200

    Cover section header/footer with unit tests

---
+++ WARPServicesTests/TableViewSectionHeaderTests.swift
@@ -0,0 +1,50 @@
+//
+//  TableViewSectionHeaderTests.swift
+//  WARPServices
+//
+//  Created by Oleksii on 31/03/2017.
+//  Copyright © 2017 Oleksii Dykan. All rights reserved.
+//
+
+import XCTest
+import WARPServices
+
+extension Int: HeaderTitleProvider, FooterTitleProvider {}
+
+class TableViewSectionHeaderTests: XCTestCase {
+    let tableView = UITableView()
+    
+    func testSectionHeaderFooterDefaultValeus() {
+        struct DataSource: DataContaining, ItemsProviding, TableViewDataSource, CellConfiguring {
+            typealias ItemType = Int
+            let data = [1, 2, 3]
+            
+            func identifier(for indexPath: IndexPath) -> String { return String() }
+            func configure(cell: UITableViewCell, for item: Int) {}
+        }
+        
+        let dataSource = DataSource()
+        XCTAssertNil(dataSource.tableView(tableView, titleForFooterInSection: 0))
+        XCTAssertNil(dataSource.tableView(tableView, titleForHeaderInSection: 0))
+    }
+    
+    
+    func testSectionHeaderFooterRealValues() {
+        struct DataSource: DataContaining, ItemsProviding, TableViewDataSource, CellConfiguring {
+            struct ItemType: HeaderTitleProvider, FooterTitleProvider {
+                let headerTitle: String? = "header"
+                let footerTitle: String? = "footer"
+            }
+            
+            let data = [ItemType(), ItemType(), ItemType()]
+            
+            func identifier(for indexPath: IndexPath) -> String { return String() }
+            func configure(cell: UITableViewCell, for item: ItemType) {}
+        }
+        
+        let dataSource = DataSource()
+        XCTAssertEqual(dataSource.tableView(tableView, titleForFooterInSection: 0), "footer")
+        XCTAssertEqual(dataSource.tableView(tableView, titleForHeaderInSection: 0), "header")
+    }
+    
+}
