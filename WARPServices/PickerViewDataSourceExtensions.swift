44584e4d492f82727e735a7b1f6bc065ad448498
Oleksii Dykan <alick_bass@mail.ru>
2017-08-29 04:54:52 -0700

    Refactor picker data source to new naming convention

--- WARPServices/PickerViewDataSourceExtensions.swift
+++ WARPServices/PickerViewDataSourceExtensions.swift
@@ -8,13 +8,13 @@
 
 import Foundation
 
-public extension PickerViewDataSource where Self: DataContaining, Self.CollectionType.IndexDistance == Int, Self.CollectionType.Index == Int {
+public extension PickerViewDataSourceType where Self: ItemsProviding {
     public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return data.count
+        return numberOfElements
     }
 }
 
-public extension PickerViewDataSource where Self: DataContaining, Self.CollectionType.IndexDistance == Int, Self.CollectionType.Index == Int, Self.CollectionType.Iterator.Element: DataContaining, Self.CollectionType.Iterator.Element.CollectionType.IndexDistance == Int, Self.CollectionType.Iterator.Element.CollectionType.Index == Int {
+public extension PickerViewDataSourceType where Self: DataContaining, Self.CollectionType.IndexDistance == Int, Self.CollectionType.Index == Int, Self.CollectionType.Iterator.Element: DataContaining, Self.CollectionType.Iterator.Element.CollectionType.IndexDistance == Int, Self.CollectionType.Iterator.Element.CollectionType.Index == Int {
     public func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return data.count
     }
